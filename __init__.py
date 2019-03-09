6e099f19d1b792108cdeb83a57b3f9e8d66de532
Daniel Russell-Brain <daniel.rb@hotmail.co.uk>
2019-03-09 21:47:05 +0000

    created the basis for a sentiment analysis module

---
+++ __init__.py
@@ -0,0 +1,50 @@
+# Creating our text classifier for sentiment analysis
+# Is the message positive or negative?
+# Binary classifier
+
+import nltk
+import random
+from nltk.corpus import movie_reviews, stopwords
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+import numpy as np
+
+stopwords = set(stopwords.words('english'))
+punctuation = '.\'\"-,;:?'
+
+
+documents = []
+
+for category in movie_reviews.categories():
+    for fileid in movie_reviews.fileids(category):
+        word_list = []
+        for word in list(movie_reviews.words(fileid)):
+            if word not in stopwords and word not in punctuation:
+                word_list.append(word)
+        documents.append((np.array(word_list), category))
+
+all_words = []
+for w in movie_reviews.words():
+    all_words.append(w.lower())
+
+all_words = nltk.FreqDist(all_words)
+
+word_features = list(all_words.keys())[:3000]
+
+train_set, test_set = train_test_split(documents)
+
+print(train_set[:10])
+
+def find_features(document):
+    words = set(document)
+    features = {}
+    for word in word_features:
+        features[word] = (word in words)
+
+    return features
+
+
+print(find_features(movie_reviews.words('neg/cv000_29416.txt')))
+
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
